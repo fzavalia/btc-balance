ecf409a5c1835fdc159e4e2dcf536f104a81c356
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2020-09-29 19:36:33 -0300

    Support multiple coins

--- index.js
+++ index.js
@@ -1,67 +1,69 @@
-const axios   = require("axios").default;
-const chalk   = require("chalk")
-const cheerio = require("cheerio")
-const fs      = require("fs")
-const path    = require("path")
+const axios = require("axios").default;
+const chalk = require("chalk");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const path = require("path");
 
-async function getUsdArsPrice() {
-
-  const url         = "https://www.dolarhoy.com/cotizaciondolarblue";
-  const res         = await axios.get(url);
-  const $           = cheerio.load(res.data)
-  const [buy, sell] = $("span.pull-right").map((_, e) => parseFloat($(e).text().slice(2))).get()
-
-  return buy + (sell - buy) / 2
+async function coinGeckoPrice(coin) {
+  const url = `https://api.coingecko.com/api/v3/coins/${coin}`;
+  const res = await axios.get(url);
+  return res.data.market_data.current_price.usd;
 }
 
-async function getBtcUsdPrice() {
+const prices = {
+  bitcoin: () => coinGeckoPrice("bitcoin"),
+  tether: () => coinGeckoPrice("tether"),
+  blue: async () => {
+    const url = "https://www.dolarhoy.com/cotizaciondolarblue";
+    const res = await axios.get(url);
+    const $ = cheerio.load(res.data);
+    const [buy, sell] = $("span.pull-right")
+      .map((_, e) => parseFloat($(e).text().slice(2)))
+      .get();
 
-  const url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-  const res = await axios.get(url)
+    return buy + (sell - buy) / 2;
+  },
+};
 
-  return res.data.bpi.USD.rate_float
+async function render() {
+  const blue = await prices.blue();
+
+  const dataPath = path.resolve(__dirname, "data.json");
+  const dataJson = await fs.promises.readFile(dataPath, { encoding: "utf-8" });
+  const data = JSON.parse(dataJson);
+
+  const hydratedCoins = await Promise.all(
+    data.coins.map((coin) =>
+      prices[coin.name]().then((price) => ({
+        ...coin,
+        price,
+        priceArs: price * blue,
+        value: price * coin.amount,
+        valueArs: price * coin.amount * blue,
+      }))
+    )
+  );
+
+  const invested = data.expenses.reduce((acc, next) => acc + next, 0);
+  const investedValue = hydratedCoins.reduce((acc, next) => acc + next.valueArs, 0);
+  const difference = investedValue - invested;
+  const color = difference > 0 ? chalk.green : chalk.red;
+
+  console.log();
+  console.log(`Blue:       ${color.blue(blue.toFixed(2))}`);
+  console.log(`Invested:   ${invested.toFixed(2)}`);
+  console.log(`Value:      ${investedValue.toFixed(2)}`);
+  console.log(`Difference: ${color(difference.toFixed(2))}`);
+  console.log(`Percentage: ${color(((Math.abs(difference) * 100) / invested).toFixed(2))}`);
+  console.log();
+  console.table(
+    hydratedCoins.map((coin) => ({
+      name: coin.name,
+      "price USD/ARS": `${coin.price.toFixed(2)} / ${coin.priceArs.toFixed(2)}`,
+      amount: `${coin.amount}`,
+      "value USD/ARS": `${coin.value.toFixed(2)} / ${coin.valueArs.toFixed(2)}`,
+    }))
+  );
 }
 
-async function loadData() {
-
-  const dataPath = path.resolve(__dirname, 'data.json')
-  const data     = await fs.promises.readFile(dataPath, { encoding: 'utf-8' })
-
-  return JSON.parse(data)
-}
-
-function getPercentage(final, invested) {
-  return final > invested ? (final / invested - 1) * 100 : (1 - final / invested) * 100
-}
-
-async function renderBalance() {
-
-  const data        = await loadData()
-  const investedArs = data.expenses.reduce((acc, next) => acc + next, 0)
-  const usdArsPrice = await getUsdArsPrice()
-  const btcUsdprice = await getBtcUsdPrice()
-  const finalArs    = data.btc * btcUsdprice * usdArsPrice
-  const difference  = finalArs - investedArs
-  const color       = difference > 0 ? chalk.green : chalk.red
-
-  console.log(`
-My BTC          |           |    ${data.btc}
-                |           |
-USD Price       |    ARS    |    ${usdArsPrice.toFixed(2)}
-                |           |
-BTC Price       |    ARS    |    ${(btcUsdprice * usdArsPrice).toFixed(2)}
-                |    USD    |    ${btcUsdprice.toFixed(2)}
-                |           |
-Invested        |    ARS    |    ${investedArs.toFixed(2)}
-                |    USD    |    ${(investedArs / usdArsPrice).toFixed(2)}
-                |           |
-Final           |    ARS    |    ${finalArs.toFixed(2)}
-                |    USD    |    ${(finalArs / usdArsPrice).toFixed(2)}
-                |           |
-Diff            |    ARS    |    ${color(`${difference.toFixed(2)} (%${getPercentage(finalArs, investedArs).toFixed(2)})`)}
-                |    USD    |    ${color((difference / usdArsPrice).toFixed(2))}
-                |           |
-`)
-}
-
-renderBalance()
+render();
